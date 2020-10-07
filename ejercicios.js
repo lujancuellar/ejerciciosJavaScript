66318066f9f1775d666836b970b880b79591bbfd
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-07 15:02:37 -0300

    Añadi el primer ejercicio

---
+++ ejercicios.js
@@ -0,0 +1,27 @@
+
+function DiaDeSemana(dia) {
+    switch (dia) {
+        case "1":
+            console.log("Lunes");
+            break;
+        case "2":
+            console.log("Martes");
+            break;
+        case "3":
+            console.log("Miercoles");
+            break;
+        case "4":
+            console.log("Jueves");
+            break;
+        case "5":
+            console.log("Viernes");
+            break;
+        case "6":
+        case "7":
+                console.log("Fin de semana");
+            break;
+         default:
+                console.log("Error");
+    }
+    document.getElementById('dia');
+}
