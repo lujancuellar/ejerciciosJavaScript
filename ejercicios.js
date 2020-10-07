9cb3aaff0870aef748f8cbff10ccf92777b16e45
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-07 15:19:09 -0300

    Correcion de errores

--- ejercicios.js
+++ ejercicios.js
@@ -23,5 +23,5 @@ function DiaDeSemana(dia) {
          default:
                 console.log("Error");
     }
-    document.getElementById('dia');
+    document.getElementById('resultado').innerHTML;
 }
