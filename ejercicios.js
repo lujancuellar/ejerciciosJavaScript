01e87a70dd28b6535a111ab7f594c20dd682d60f
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-09 15:46:21 -0300

    Añadi el ultimo ejercicio que faltaba del martes

--- ejercicios.js
+++ ejercicios.js
@@ -196,3 +196,34 @@ function dibujarNumero (num) {
   function mostrarFecha() {
       document.getElementById("fecha").innerHTML = Date();
   }
+
+
+  //promedio de numeros.Este ejercicio me dio Daniel Evangelisto y este es su codigo. No pude resolverlo sola. Trato de entender que hizo.
+
+  function promedio(numero){
+    let cantidad   = Number(numero);
+        contador   = cantidad;
+        array      = [];
+        suma       = 0;
+
+        for (let i = 0; i < cantidad; i++) {
+            
+            if (i === 0) {
+                let ingreso = prompt(`Ingrese numero`);
+                array.push(Number(ingreso));
+                contador -= 1;
+            }else{
+                let ingreso = prompt(`restan ${contador} ingreso/s.`);
+            
+                array.push(Number(ingreso));
+                contador -= 1;
+            }
+            
+        }
+        for (let j = 0; j < array.length; j++) {
+            suma += array[j];
+            
+        }
+        document.getElementById("resultado").innerHTML = `El promedio de numeros ingresados es de: <br> ${suma/cantidad}`;
+    
+}
